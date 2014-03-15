0f820d181d1faa824a10fb4a9ba3c6f8f1fb2314
Umair Naveed <unaveed@gmail.com>
2014-03-14 22:33:22 -0600

    Initial commit

---
+++ service_lane/source.cpp
@@ -0,0 +1,17 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+using namespace std;
+
+int main(){
+	// Holds input stream	
+	string input;
+	int arr_size, loops;
+	int* widths;	// Remember to delete []widths;
+		
+	getline(cin, input);
+
+	cout << input << endl;
+	return 0;
+}
