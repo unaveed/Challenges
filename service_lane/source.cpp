ad821b861852a2b06ee8a68c1fc6a3c6e363bf2a
Umair Naveed <unaveed@gmail.com>
2014-03-14 23:45:20 -0600

    Done with reading input

--- service_lane/source.cpp
+++ service_lane/source.cpp
@@ -7,11 +7,41 @@ using namespace std;
 int main(){
 	// Holds input stream	
 	string input;
-	int arr_size, loops;
+	// Array size, number of test cases, and a counter to keep
+	// track of our position in the input
+	int arr_size, loops, counter;
+	// The widths of the enter/exit lanes
 	int* widths;	// Remember to delete []widths;
-		
-	getline(cin, input);
+	// Holds tests cases
+	vector<string> cases;	
+	
+	counter = 1;
+	while(true){
+		getline(cin, input);	
+		if(input.empty())
+			break;
 
-	cout << input << endl;
+		// First run, initialize variables
+		if(counter == 1){
+			arr_size = input[0]- '0'; 
+			loops = input[2] - '0';
+
+			widths = new int[arr_size];
+		}
+		// Second run, initialize the array
+		else if(counter == 2){
+			for(int i = 0; i < arr_size*2; i+=2)
+				widths[i] = input[i] - '0';
+		}
+		// Add each test case to the cases vector
+		else {
+			cases.push_back(input);	
+		}
+
+		// keeps track of which line of the input is being read
+		counter++;
+	}
+
+	cout << "Reached end" << endl;
 	return 0;
 }
